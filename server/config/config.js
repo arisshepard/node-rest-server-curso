49d5b726be841a523a233c0327aa9ab2d26bc638
Maria <mfiremetal@gmail.com>
2020-11-17 17:01:09 +0100

    Prueba node_env 2

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ===========================
 let urlDB;
 console.log('El dato: ', process.env.NODE_ENV);
-if ((process.env.NODE_ENV = 'dev')) {
+if (process.env.NODE_ENV === 'dev') {
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 	urlDB =
